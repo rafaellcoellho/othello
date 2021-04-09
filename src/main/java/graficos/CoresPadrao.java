f0390d252ae3763d5f3a6deee4f8f1b36150180a
rafaellcoellho <rafaellcoellho@gmail.com>
2021-04-09 00:19:11 -0300

    CoresPadrao: adiciona cor do fundo do chat e do botao de desistir

--- src/main/java/graficos/CoresPadrao.java
+++ src/main/java/graficos/CoresPadrao.java
@@ -6,7 +6,9 @@ public enum CoresPadrao {
 
     FUNDO(0.192f, 0.180f, 0.168f), //#312e2b
     QUADRADO(0.462f, 0.588f, 0.337f), //#769656
-    BOTAO(0.498f, 0.650f, 0.313f); //#7fa650
+    BOTAO_PASSAR_TURNO(0.498f, 0.650f, 0.313f), //#7fa650
+    FUNDO_CHAT(0.694f, 0.694f, 0.690f), //#b1b1b0
+    BOTAO_DESISTIR(0.701f, 0.203f, 0.188f); //#b33430
 
     public Color cor;
 
