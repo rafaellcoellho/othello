30757d8ac19bba1a3d88e66dd2498f8b274edce6
rafaellcoellho <rafaellcoellho@gmail.com>
2021-04-09 16:36:23 -0300

    CoresPadrao(graficos): adiciona nova cor de quadrado

--- src/main/java/graficos/CoresPadrao.java
+++ src/main/java/graficos/CoresPadrao.java
@@ -5,7 +5,8 @@ import com.badlogic.gdx.graphics.Color;
 public enum CoresPadrao {
 
     FUNDO(0.192f, 0.180f, 0.168f), //#312e2b
-    QUADRADO(0.462f, 0.588f, 0.337f), //#769656
+    QUADRADO_ESCURO(0.462f, 0.588f, 0.337f), //#769656
+    QUADRADO_CLARO(0.933f, 0.933f, 0.823f), //#eeeed2
     BOTAO_PASSAR_TURNO(0.498f, 0.650f, 0.313f), //#7fa650
     FUNDO_CHAT(0.694f, 0.694f, 0.690f), //#b1b1b0
     BOTAO_DESISTIR(0.701f, 0.203f, 0.188f); //#b33430
