634c28f2ed0069c8b9e8070eef41b31f4846d34a
rafaellcoellho <rafaellcoellho@gmail.com>
2021-04-14 04:57:20 -0300

    rede: cria classe base de comunicação e usa linha de comando para rodar cliente e servidor

--- src/main/java/graficos/Othello.java
+++ src/main/java/graficos/Othello.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import graficos.telas.TelaDoTabuleiro;
 import logica.Logica;
+import rede.Comunicacao;
 
 public class Othello extends Game {
 
@@ -14,6 +15,22 @@ public class Othello extends Game {
   public Skin estilo;
   public ShapeRenderer shapeRenderer;
   public Logica estado;
+  public Comunicacao comunicacao;
+
+  public Othello(String arg) {
+    Comunicacao.Papel papel = arg.equals("servidor") ? Comunicacao.Papel.SERVIDOR : Comunicacao.Papel.CLIENTE;
+    comunicacao = new Comunicacao(papel, "localhost", 9090);
+
+    if (papel.equals(Comunicacao.Papel.SERVIDOR)) {
+      System.out.println("Esperando conexão...");
+      comunicacao.esperarConexao();
+      System.out.println("Sucesso...");
+    } else {
+      System.out.println("Tentando conexão...");
+      comunicacao.conectar();
+      System.out.println("Sucesso...");
+    }
+  }
 
   @Override
   public void create() {
